cc02922884affebaa1c39112c6e77aca64ea821b
Braden Belcher <jbbelcher53@gmail.com>
2023-03-16 11:48:03 -0500

    New: add pagination

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ This repository is for red technologies interview
 - Tailwindcss
 - Jest/React Testing Library
 - Radix UI
+- React Table v8
 - Automatic workflows & deployment using Github Actions
 - Utilizing Context for draft state management
 - Deployed on Vercel at: https://red-technologies-braden-belcher.vercel.app/
--- src/app/components/OrderTable/OrderTable.tsx
+++ src/app/components/OrderTable/OrderTable.tsx
@@ -537,7 +537,71 @@ export const OrderTable: React.FC<IOrderTable> = ({
           </tbody>
         </table>
       </div>
-
+      <div className='flex items-center gap-2'>
+        <button
+          type='button'
+          className='p-1 border rounded'
+          onClick={() => table.setPageIndex(0)}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<<'}
+        </button>
+        <button
+          type='button'
+          className='p-1 border rounded'
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          {'<'}
+        </button>
+        <button
+          type='button'
+          className='p-1 border rounded'
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>'}
+        </button>
+        <button
+          type='button'
+          className='p-1 border rounded'
+          onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+          disabled={!table.getCanNextPage()}
+        >
+          {'>>'}
+        </button>
+        <span className='flex items-center gap-1'>
+          <div>Page</div>
+          <strong>
+            {table.getState().pagination.pageIndex + 1} of{' '}
+            {table.getPageCount()}
+          </strong>
+        </span>
+        <span className='flex items-center gap-1'>
+          | Go to page:
+          <input
+            type='number'
+            defaultValue={table.getState().pagination.pageIndex + 1}
+            onChange={e => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0
+              table.setPageIndex(page)
+            }}
+            className='w-16 p-1 border rounded'
+          />
+        </span>
+        <select
+          value={table.getState().pagination.pageSize}
+          onChange={e => {
+            table.setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
       <div>
         {Object.keys(rowSelection).length} of{' '}
         {table.getPreFilteredRowModel().rows.length} Total Rows Selected
